7e380ecfbd3524c2d07568db2526ab925a35f143
muhsupianmaolana-ship-it <muhsupianmaolana@gmail.com>
2026-04-14 12:04:03 +0800

    menambah variabel alamat

--- indexx.php
+++ indexx.php
@@ -4,6 +4,7 @@ $nama = "lana";
 $umur = 20;
 $tinggi = 169.4;
 $hobi = ["futsal","melukis"];
+$alamat = "lombok timur"
 
 echo "Nama saya $nama, umur saya $umur, tinggi saya $tinggi, hobi saya $hobi[0],$hobi[1]";
 ?>

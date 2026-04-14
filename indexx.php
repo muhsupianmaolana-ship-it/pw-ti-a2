716ba77ed5fcfaa424a008888534d8f533e59418
muhsupianmaolana-ship-it <muhsupianmaolana@gmail.com>
2026-04-14 11:44:16 +0800

    update v1

---
+++ indexx.php
@@ -0,0 +1,9 @@
+<?php
+
+$nama = "lana";
+$umur = 20;
+$tinggi = 169.4;
+$hobi = ["futsal","melukis"];
+
+echo "Nama saya $nama, umur saya $umur, tinggi saya $tinggi, hobi saya $hobi[0],$hobi[1]";
+?>

e9a92c96bc6e398d0a7a0d51735ee97718d8178d
miftahulhaeri31-gif <miftahulhaeri31@gmail.com>
2026-04-28 08:25:00 +0800

    update v1

--- mteri3.php
+++ mteri3.php
@@ -1,18 +1,22 @@
-<form method = "post">
+<form method="post">
     Masukkan Angka : <input type="number" name="angka">
     <input type="submit" name="kirim" value="kirim">
-    </form>
+</form>
 
-    <?php
-if(isset($_POST["angka"])){
-$newAngka = $_POST["angka"];
-for($i = 1; $i <= $newAngka; $i++){
-    if($i % 2 != 0){
-        echo "ganjil $i <br>";
-    } else {
-        echo "genap $i <br>";
+<?php
+function cekAngkaGanjilGenap($angka) {
+    for($i = 1; $i <= $angka; $i++){
+        if($i % 2 != 0){
+            echo "ganjil $i <br>";
+        } else {
+            echo "genap $i <br>";
+        }
     }
 }
 
+// Memanggil function
+if(isset($_POST["angka"])){
+    $newAngka = $_POST["angka"];
+    cekAngkaGanjilGenap($newAngka);
 }
-    ?>
+?>
